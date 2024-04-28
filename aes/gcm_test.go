0e2a1f9862a146372def0615f1cc7e1edd3bac3b
M3DZIK <me@medzik.dev>
2024-04-28 16:37:21 +0200

    Add TestDecryptGCM

--- aes/gcm_test.go
+++ aes/gcm_test.go
@@ -1,13 +1,12 @@
 package aes_test
 
 import (
-	"encoding/hex"
 	"go.medzik.dev/crypto/aes"
 	"testing"
 )
 
 func TestEncryptGCM(t *testing.T) {
-	key, _ := hex.DecodeString("82fd4cefd6efde36171900b469bae4e06863cb70f80b4e216e44eeb0cf30460b")
+	key, _ := aes.DecodeKey("82fd4cefd6efde36171900b469bae4e06863cb70f80b4e216e44eeb0cf30460b")
 	data := []byte("hello world")
 
 	encryptedData, err := aes.EncryptGCM(key, data)
@@ -21,6 +20,21 @@ func TestEncryptGCM(t *testing.T) {
 	}
 
 	if string(decryptedData) != string(data) {
+		t.Error("decrypted data is not the same as the original data")
+	}
+}
+
+func TestDecryptGCM(t *testing.T) {
+	key, _ := aes.DecodeKey("82fd4cefd6efde36171900b469bae4e06863cb70f80b4e216e44eeb0cf30460b")
+	cipherText := "0996c65a72a60e748415dc6d32da1d4dcb65f41c71d4bec9554424218839b5d4b9d9195e5eea9d"
+	decodedCipherText, _ := aes.DecodeCipherText(cipherText)
+
+	clearText, err := aes.DecryptGCM(key, decodedCipherText)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(clearText) != "hello world" {
 		t.Error("decrypted data is not the same as the original data")
 	}
 }
